ef684874b239f47ed6531772ac6aa01ab6b0c290
wjackson2112 <wjackson2112@gmail.com>
2023-07-19 10:23:15 -0400

    Turn shuffling back on

--- spider/source/SPSnapValidatorFourSuits.cpp
+++ spider/source/SPSnapValidatorFourSuits.cpp
@@ -133,13 +133,13 @@ void SPSnapValidatorFourSuits::initialSetup(Scene *scene)
             }
 //        }
     }
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
-//    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
+    std::shuffle(std::begin(cardSet), std::end(cardSet), rng);
 
     for(int i = 0; i < 10; i++)
     {
